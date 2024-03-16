bdd9f26c8e4fd6ba847a41b4ba390929e81a23a9
Tanveer123456 <tanveerchougule1@gmail.com>
2024-03-16 10:51:40 +0530

    Project created

---
+++ src/main/java/com/ttpl/maintenance/maintenanceapp/MaintenanceappApplication.java
@@ -0,0 +1,13 @@
+package com.ttpl.maintenance.maintenanceapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaintenanceappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaintenanceappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ttpl/maintenance/maintenanceapp/MaintenanceappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ttpl.maintenance.maintenanceapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaintenanceappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
